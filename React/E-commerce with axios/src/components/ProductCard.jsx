b6f16594e896f6788d0069b0a65f4aac4c02d43c
Abdul-Hannan786 <abdulhannanhere@gmail.com>
2025-06-26 18:22:50 +0500

    project completed

--- React/E-commerce with axios/src/components/ProductCard.jsx
+++ React/E-commerce with axios/src/components/ProductCard.jsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ item }) => {
-  const { price, thumbnail, title, category } = item;
+  const { price, thumbnail, title, category, id } = item;
 
   return (
-    <div className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md">
-      <a className="block relative h-48 rounded overflow-hidden">
-        <img
-          alt="ecommerce"
-          className="object-cover object-center w-full h-full block"
-          src={thumbnail}
-        />
-      </a>
-      <div className="mt-4">
-        <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
-          {category}
-        </h3>
-        <h2 className="text-gray-900 title-font text-lg font-medium">
-         {title}
-        </h2>
-        <p className="mt-1">${price}</p>
+    <Link to={`/products/${id}`} className="lg:w-1/4 md:w-1/2 p-4 w-full shadow-md">
+      <div>
+        <div className="block relative h-48 rounded overflow-hidden">
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src={thumbnail}
+          />
+        </div>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+            {category}
+          </h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            {title}
+          </h2>
+          <p className="mt-1">${price}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
